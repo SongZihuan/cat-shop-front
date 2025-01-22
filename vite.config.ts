87a227a812779e9a950c72e374c9a4bf07be2e40
宋子桓🌈 <songzihuan@song-zh.com>
2025-01-22 17:55:05 +0800

    更新打包路径配置
    将默认打包路径从 './' 修改为 '/'，以便更好地支持不同环境下的资源访问。
    
    Signed-off-by: 宋子桓🌈 <songzihuan@song-zh.com>

--- vite.config.ts
+++ vite.config.ts
@@ -95,7 +95,7 @@ export default ({ mode }: ConfigEnv): UserConfig => {
       svgLoader(),
       visualizerPlugin
     ],
-    base: VITE_ASSETS_BASE || './', // 设置打包路径
+    base: VITE_ASSETS_BASE || '/', // 设置打包路径
     server: {
       host: '0.0.0.0',
       proxy: {
